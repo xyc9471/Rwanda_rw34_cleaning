60799f987b626b28271abf2157fabe88f3b2b9b5
xyc9471 <46945444+xyc9471@users.noreply.github.com>
2019-03-04 15:25:27 -0500

    Master R-Script for Cleaning Module B and B1

---
+++ hhrw_modB_B1_master.R
@@ -0,0 +1,48 @@
+#------------------------------------------------------------------------------ #
+#                                                                               #
+#                                    DIME                                       #
+#                   Data Cleaning for Rwanda Household Info                     #
+#                             for Module B and B1                               #
+#                                MASTER SCRIPT                                  #
+#                                                                               #
+#                                                                               #
+#------------------------------------------------------------------------------ #
+
+# PURPOSE:    Set-up configurations and run scripts that are used to clean data 
+#             collected for Rwanda Irrigation Project
+# NOTES:      
+
+# WRITTEN BY: Yuchen Xiang
+
+#                                                     Last modified in Nov 2018
+
+# PART 0: Clear boiler plate --------------------------------------------------
+
+  rm(list=ls())
+
+# PART 1: Setup work directory ------------------------------------------------
+
+  projectFolder     <- file.path("C:/Users/WB546716/Documents/GitHub/Rwanda_rw34_cleaning")
+  dataWorkFolder    <- file.path(projectFolder, "DataWork")
+  followupFolder    <- file.path(dataWorkFolder, "followup1")
+  Data              <- file.path(followupFolder, "DataSets")
+  DeidentifiedData  <- file.path(Data, "Deidentified")
+  InterData         <- file.path(Data, "Intermediate")
+
+# PART 2: Load packages -------------------------------------------------------
+
+  library(haven)
+  library(sjmisc)
+  library(stringr)
+  library(reshape)
+  library(reshape2)
+  library(data.table)
+  library(gtools)
+  library(dplyr)
+
+# PART 3: Load data files -----------------------------------------------------
+
+  hh_rw34 <- read_dta(file.path(DeidentifiedData,
+                                "fup1_rw34_incoming_data_toclean.dta"))
+  
+  
